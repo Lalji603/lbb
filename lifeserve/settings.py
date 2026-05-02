5f556e9da6df7c13c5830aa3ea9f08166dbf54a7
Lalji Patel <lalji7047@gmail.com>
2026-05-02 08:55:46 +0530

    Update settings.py

--- lifeserve/settings.py
+++ lifeserve/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-a0_n7*=mwvcqx4n9p9ktx%bl9bbq09%_tye^afp*qd9i=tn%&3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'testserver', '0.0.0.0']
+ALLOWED_HOSTS = ['*']
 
 # CSRF Trusted Origins for browser preview
 CSRF_TRUSTED_ORIGINS = [
